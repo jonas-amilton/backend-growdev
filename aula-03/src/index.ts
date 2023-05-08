b268ff185463b850bd0e854eadff4d95efd5b225
Jonas Silva <90778653+jonas-amilton@users.noreply.github.com>
2023-05-08 17:48:03 -0300

    exercicio 1 :fire:

--- aula-03/src/index.ts
+++ aula-03/src/index.ts
@@ -1,58 +1,10 @@
-// // Paradigma IMPERATIVO
-// console.log("Olá mundo!!!!!");
+import { Normal, Vip, Camarote } from "./ingresso";
 
-// var nota1: number = 10;
-// var nota2: number = 7;
+const ingressoNormal = new Normal(50);
+ingressoNormal.imprimeValor(); // Valor do ingresso normal: R$ 50.00
 
-// var media = (nota1 + nota2) / 2;
+const ingressoVIP = new Vip(100, 50);
+ingressoVIP.imprimeValor(); // Valor do ingresso VIP: R$ 150.00
 
-// console.log(media);
-
-// // Paradigma PROCEDURAL
-// function calcularMedia(nota1: number, nota2: number) {
-//     return (nota1 + nota2) / 2;
-// }
-
-// media = calcularMedia(5, 9);
-// console.log(media);
-
-// media = calcularMedia(7, 2);
-// console.log(media);
-
-// // Paradigma FUNCIONAL
-// // [1, 2, 3, 4]
-// //     .map((item) => {
-// //         console.log(item);
-// //         return item;
-// //     })
-// //     .map((item) => item)
-// //     .filter();
-
-// // Paradigma Orientado a Objetos
-
-import { Casa } from "./casa";
-
-let casa1 = new Casa();
-casa1.cor = "branco";
-casa1.qtdJanelas = 10;
-casa1.janelasAbertas = 0;
-casa1.altura = 50;
-
-let casaDePraia = new Casa();
-casaDePraia.cor = "amarelo";
-casaDePraia.janelasAbertas = 2;
-casaDePraia.qtdJanelas = 10;
-casaDePraia.altura = 20;
-
-// console.log("Qtd de janelas abertas: " + casa1.janelasAbertas);
-
-// casa1.abrirJanela();
-
-// console.log("Qtd de janelas abertas depois do método: " + casa1.janelasAbertas);
-
-// casa1.informarAltura();
-// casaDePraia.informarAltura();
-
-casaDePraia.alterarCor("cinza");
-casa1.alterarCor("cinza");
-console.log(casaDePraia);
+const ingressoCamarote = new Camarote(150, 100);
+ingressoCamarote.imprimeValor(); // Valor do ingresso Camarote: R$ 250.00
