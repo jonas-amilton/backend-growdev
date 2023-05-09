1d8bbf1330a2315485b301ed7243485aebb05fc0
Jonas Silva <jonasgenro@gmail.com>
2023-05-09 15:22:25 -0300

    :sparkles: ex1 e ex2

--- aula-03/src/index.ts
+++ aula-03/src/index.ts
@@ -1,10 +1,37 @@
-import { Normal, Vip, Camarote } from "./ingresso";
+import { Camarote, Normal, Vip } from "./ex1";
+import { Imovel, Novo, Velho } from "./ex2";
 
-const ingressoNormal = new Normal(50);
-ingressoNormal.imprimeValor(); // Valor do ingresso normal: R$ 50.00
+function exercicio1() {
+    const camarote = new Camarote(20, 5);
 
-const ingressoVIP = new Vip(100, 50);
-ingressoVIP.imprimeValor(); // Valor do ingresso VIP: R$ 150.00
+    camarote.imprimirValor();
 
-const ingressoCamarote = new Camarote(150, 100);
-ingressoCamarote.imprimeValor(); // Valor do ingresso Camarote: R$ 250.00
+    const normal = new Normal(8);
+
+    normal.imprimirValor();
+
+    const vip = new Vip(12, 3);
+
+    vip.imprimirValor();
+}
+
+function exercicio2() {
+    const imovel = new Imovel("rua da alegria 21", 200);
+
+    const velho = new Velho("rua da tristeza", 300, 40);
+
+    const novo = new Novo("rua nova", 200, 3000);
+
+    // imovel.imprimirValor();
+
+    // velho.imprimirValor();
+
+    // novo.imprimirValor();
+
+    console.log(imovel.getEndereco());
+
+    console.log(imovel.endereco);
+    imovel.endereco = "novo endereco";
+}
+
+exercicio2();
